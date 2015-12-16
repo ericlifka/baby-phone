262c0e9700efcb0d03a66b6a8b0899093ee9d49d
Eric Lifka <eric.lifka@gmail.com>
2015-12-16 17:22:42 -0500

    Initial Commit

---
+++ baby phone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  baby phone
+//
+//  Created by Eric Lifka on 12/16/15.
+//  Copyright © 2015 Pookums & Pookums LTD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ baby phoneTests/baby_phoneTests.swift
@@ -0,0 +1,36 @@
+//
+//  baby_phoneTests.swift
+//  baby phoneTests
+//
+//  Created by Eric Lifka on 12/16/15.
+//  Copyright © 2015 Pookums & Pookums LTD. All rights reserved.
+//
+
+import XCTest
+@testable import baby_phone
+
+class baby_phoneTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
